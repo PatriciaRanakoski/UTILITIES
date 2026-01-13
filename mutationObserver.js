41547ab90c922ea11af829f93046a12260247f54
Patricia <ranakoskipatricia@gmail.com>
2026-01-13 11:08:19 +0000

    changes to mutation

--- mutationObserver.js
+++ mutationObserver.js
@@ -1,43 +1,43 @@
 var listeners = [],
-doc = win.document,
-MutationObserver = win.MutationObserver || win.WebKitMutationObserver,
-observer;
+    doc = win.document,
+    MutationObserver = win.MutationObserver || win.WebKitMutationObserver,
+    observer;
 
 function ready(selector, fn) {
-// Store the selector and callback to be monitored
-listeners.push({
-selector: selector,
-fn: fn
-});
-if (!observer) {
-// Watch for changes in the document
-observer = new MutationObserver(check);
-observer.observe(doc.documentElement, {
-childList: true,
-subtree: true
-});
-}
-// Check if the element is currently in the DOM
-check();
+    // Store the selector and callback to be monitored
+    listeners.push({
+        selector: selector,
+        fn: fn
+    });
+    if (!observer) {
+        // Watch for changes in the document
+        observer = new MutationObserver(check);
+        observer.observe(doc.documentElement, {
+            childList: true,
+            subtree: true
+        });
+    }
+    // Check if the element is currently in the DOM
+    check();
 }
 
 function check() {
-// Check the DOM for elements matching a stored selector
-for (var i = 0, len = listeners.length, listener, elements; i < len; i++) {
-listener = listeners[i];
-// Query for elements matching the specified selector
-elements = doc.querySelectorAll(listener.selector);
-for (var j = 0, jLen = elements.length, element; j < jLen; j++) {
-element = elements[j];
-// Make sure the callback isn't invoked with the
-// same element more than once
-if (!element.ready) {
-element.ready = true;
-// Invoke the callback with the element
-listener.fn.call(element, element);
-}
-}
-}
+    // Check the DOM for elements matching a stored selector
+    for (var i = 0, len = listeners.length, listener, elements; i < len; i++) {
+        listener = listeners[i];
+        // Query for elements matching the specified selector
+        elements = doc.querySelectorAll(listener.selector);
+        for (var j = 0, jLen = elements.length, element; j < jLen; j++) {
+            element = elements[j];
+            // Make sure the callback isn't invoked with the
+            // same element more than once
+            if (!element.ready) {
+                element.ready = true;
+                // Invoke the callback with the element
+                listener.fn.call(element, element);
+            }
+        }
+    }
 }
 
 // Expose 'ready'
@@ -46,7 +46,7 @@ win.optiReady = ready;
 (this);
 // pass the selector you want the mutation to whatch
 optiReady('', () => {
-// call your main function here
-//main_190();
+    // call your main function here
+    //main_190();
 
 });
